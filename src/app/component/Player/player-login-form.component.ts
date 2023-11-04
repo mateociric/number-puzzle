080cf76c2cbb554c6590cec9066e98dc2549f7e6
mciricprogramming <mciricprogramming@gmail.com>
2023-11-04 19:33:21 +0100

    implementing logic in player-form-component, retouching the style

--- src/app/component/Player/player-login-form.component.ts
+++ src/app/component/Player/player-login-form.component.ts
@@ -1,4 +1,5 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute, Router } from "@angular/router";
 
 @Component({
     selector: 'player-login-form',
@@ -6,6 +7,31 @@ import { Component } from "@angular/core";
     styleUrls: ['./player-login-form.component.css']
 })
 
-export class PlayerLoginFormComponent {
+export class PlayerLoginFormComponent implements OnInit {
+    title: string = '';
+    btnText: string = '';
+    link: string = '';
+
+    constructor(private route: ActivatedRoute, private router: Router) {
+
+    }
+
+    ngOnInit(): void {
+        this.route.url.subscribe(pathname => {
+            const currPathname = pathname.join('');
+            if (currPathname === 'Login') {
+                this.title = 'Login';
+                this.btnText = 'LOGIN';
+                this.link = "Don't have account? Signup";
+            } else {
+                this.title = 'Signup';
+                this.btnText = 'SIGNUP';
+                this.link = 'Back to login';
+            }
+        });
+    }
+    onRedirect() {
+        this.link === "Don't have account? Signup" ? this.router.navigate(['/Signup']) : this.router.navigate(['/Login']);
+    }
 
 }
